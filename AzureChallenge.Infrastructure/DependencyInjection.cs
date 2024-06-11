87d5204ca93497a736090c2c0503a1c4702f9e0e
williamfm <santifon15@gmail.com>
2024-06-11 06:30:52 -0500

    Configure auth with JWT

--- AzureChallenge.Infrastructure/DependencyInjection.cs
+++ AzureChallenge.Infrastructure/DependencyInjection.cs
@@ -4,18 +4,46 @@ using AzureChallenge.Infrastructure.Security;
 using AzureChallenge.Infrastructure.Persistence;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
 
 namespace AzureChallenge.Infrastructure;
 
 public static class DependencyInjection
 {
-  public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
-  {
-    services.Configure<JwtSettings>(configuration.GetSection("Jwt"));
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddAuth(configuration);
 
-    services.AddSingleton<IJwtGenerator, JwtGenerator>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
 
-    services.AddScoped<IUserRepository, UserRepository>();
-    return services;
-  }
+    public static IServiceCollection AddAuth(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SectionName, jwtSettings);
+        services.AddSingleton(Options.Create(jwtSettings));
+
+        services.AddSingleton<IJwtGenerator, JwtGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(options =>
+          {
+              options.TokenValidationParameters = new TokenValidationParameters()
+              {
+                  ValidateIssuer = true,
+                  ValidateAudience = true,
+                  ValidateLifetime = true,
+                  ValidateIssuerSigningKey = true,
+                  ValidIssuer = jwtSettings.Issuer,
+                  ValidAudience = jwtSettings.Audience,
+                  IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key))
+              };
+          });
+
+        return services;
+    }
 }
