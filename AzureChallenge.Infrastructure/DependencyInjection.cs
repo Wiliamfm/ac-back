26c999b97cd6e6423ee74226935dd557f0ba0ab2
williamfm <santifon15@gmail.com>
2024-06-25 08:40:27 -0500

    Add secrets config

--- AzureChallenge.Infrastructure/DependencyInjection.cs
+++ AzureChallenge.Infrastructure/DependencyInjection.cs
@@ -21,6 +21,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
+        services.AddSecrets(configuration);
         services.AddAuth(configuration);
         services.AddDb(configuration);
 
